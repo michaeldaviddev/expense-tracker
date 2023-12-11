c0f5474087ca6648b01096cbb3f5da2828036840
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-12-10 23:28:08 -0500

    Initial Commit

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+//  Created by Michael David Machacuay on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
