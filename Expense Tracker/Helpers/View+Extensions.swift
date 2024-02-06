b5cb4601dde6457e3f003325352073b9b3283fed
Michael David Machacuay <mdavidmachacuay@icloud.com>
2024-02-06 17:57:59 -0500

    Building Animated Sticky Header View

---
+++ Expense Tracker/Helpers/View+Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  View+Extentions.swift
+//  Expense Tracker
+//
+//  Created by Michael David Machacuay on 6/02/24.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func hSpacing(_ aligment: Alignment = .center) -> some View {
+        self
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: aligment)
+    }
+    
+    @ViewBuilder
+    func vSpacing(_ aligment: Alignment = .center) -> some View {
+        self
+        .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: aligment)
+    }
+    
+    var safeArea: UIEdgeInsets {
+        if let windowScene = (UIApplication.shared.connectedScenes.first as? UIWindowScene) {
+            return windowScene.keyWindow?.safeAreaInsets ?? .zero
+        }
+        
+        return .zero
+    }
+    
+    func format(date: Date, format: String) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.string(from: date)
+    }
+}
