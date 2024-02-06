9368d6f1a27f13fec4e96a46deb0c42ce9aea3b8
Michael David Machacuay <mdavidmachacuay@icloud.com>
2024-02-06 13:35:05 -0500

    Setup

---
+++ Expense Tracker/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  Expense Tracker
+//
+//  Created by Michael David Machacuay on 6/02/24.
+//
+
+import SwiftUI
+
+enum Category: String {
+    case income = "Income"
+    case expense = "Expense"
+}
